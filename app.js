5b15cf907ca8ef8304eb8bca55c3c5d7011d70f2
NgTienTrong <trongtiens96@gmail.com>
2022-01-14 23:14:49 +0700

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {math, loghoang} = require('./applicationlog')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/user', (req, res) => {
+    math()
+    loghoang()
     return res.send("abc")
 })
 
---
+++ applicationlog.js
@@ -0,0 +1,12 @@
+const math = () => {
+    console.log(2)
+}
+
+const loghoang = () => {
+    console.log('hoang')
+}
+
+module.exports = {
+    math,
+    loghoang
+}
